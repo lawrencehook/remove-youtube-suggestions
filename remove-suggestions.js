7c2ddae4cd9e63370c05a275552fa56e546f95c7
Lawrence Hook <lawrencehook@gmail.com>
2018-12-17 23:10:47 -0500

    fix bug that prevented commenting

--- remove-suggestions.js
+++ remove-suggestions.js
@@ -54,13 +54,17 @@ function onGotSidebarSetting(item) {
   if (removeSidebar) {
     let sheets = document.styleSheets;
     let display_none = " { display: none !important; }";
+
     sheets[0].insertRule("ytd-compact-video-renderer.style-scope" + display_none);
     sheets[0].insertRule("ytd-compact-radio-renderer.style-scope" + display_none);
+
     sheets[0].insertRule("ytd-image-companion-renderer.style-scope" + display_none);
     sheets[0].insertRule("ytd-compact-playlist-renderer.style-scope" + display_none);
     sheets[0].insertRule("a.ytd-action-companion-renderer" + display_none);
+    sheets[0].insertRule("#google_companion_ad_div" + display_none);
+
     sheets[0].insertRule("#upnext" + display_none);
-    sheets[0].insertRule("paper-button.style-scope" + display_none); // show more button
+    sheets[0].insertRule("paper-button.yt-next-continuation" + display_none); // show more button
   }
 }
 
