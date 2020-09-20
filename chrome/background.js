8b57695879185bdd1c09c7c54ab07b8a7be97606
Lawrence Hook <lawrencehook@gmail.com>
2020-09-20 18:53:49 -0400

    Implement the reload button in chrome

--- chrome/background.js
+++ chrome/background.js
@@ -1,8 +1,21 @@
-// https://stackoverflow.com/questions/35882089/popup-is-not-appearing-when-used-page-action
-const activatePageAction = (request, sender, sendResponse) => {
-  if (request.message === "activate_icon") {
-  	chrome.pageAction.show(sender.tab.id);
-  }
-}
+// Handles message receives
 
-chrome.extension.onMessage.addListener(activatePageAction);
+chrome.extension.onMessage.addListener((request, sender, sendResponse) => {
+
+  // Options with "page action"
+  // https://stackoverflow.com/questions/35882089/popup-is-not-appearing-when-used-page-action
+  if (request.message === "activate_icon") {
+    chrome.pageAction.show(sender.tab.id);
+  }
+
+  // Reload button
+  if (request.message === "reload_page") {
+    const activeTab = { active: true, currentWindow: true };
+    chrome.tabs.query(activeTab, tabs => {
+      tabs.forEach(tab => {
+        chrome.tabs.update(tab.id, { url: tab.url });
+      });
+    });
+  }
+
+});
--- chrome/reload.js
+++ chrome/reload.js
@@ -1,10 +1,5 @@
-const reloadButton = document.getElementById("reload_button");
-reloadButton.addEventListener("click", reload);
-
-console.log("hi");
-
-async function reload() {
-	chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-		console.log(tabs);
-	});
+const sendReloadMessage = () => {
+	chrome.runtime.sendMessage({ "message": "reload_page" });
 }
+const reloadButton = document.getElementById("reload_button");
+reloadButton.addEventListener("click", sendReloadMessage);
