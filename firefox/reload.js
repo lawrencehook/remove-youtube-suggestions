229c34ecdc1a83083265350f7f00d93506f20118
Lawrence Hook <lawrencehook@gmail.com>
2021-01-09 14:19:18 -0500

    Only reload the active tab of the _current window_.

--- firefox/reload.js
+++ firefox/reload.js
@@ -2,6 +2,6 @@ const reloadButton = document.getElementById("reload_button");
 reloadButton.addEventListener("click", reload);
 
 async function reload() {
-  const tabs = await browser.tabs.query({ active: true });
+  const tabs = await browser.tabs.query({ active: true, currentWindow: true });
   tabs.forEach(tab => browser.tabs.reload(tab.id));
 }
