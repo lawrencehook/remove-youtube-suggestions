c7436e639013a3670c21982f5b327cbeecab9172
Lawrence Hook <lawrencehook@gmail.com>
2021-01-23 12:35:13 -0500

    Update chrome version to use pure css method

---
+++ chrome/js/options.js
@@ -0,0 +1,40 @@
+const HTML = document.documentElement;
+const DEFAULT_SETTINGS = {
+  "remove_homepage": true,
+  "remove_sidebar": true,
+  "remove_end_of_video": true,
+  "remove_trending": false,
+  "remove_comments": false
+}
+
+// Make checkboxes reflect local settings
+document.addEventListener("DOMContentLoaded", () => {
+  chrome.storage.local.get(localSettings => {
+    console.log(localSettings);
+    Object.keys(localSettings).forEach(settingKey => {
+      if (!Object.keys(DEFAULT_SETTINGS).includes(settingKey)) return;
+      document.getElementById(settingKey).checked = localSettings[settingKey];
+    });
+  });
+});
+
+// Handle check/uncheck events
+Object.keys(DEFAULT_SETTINGS).forEach(settingKey => {
+  const settingCheckbox = document.getElementById(settingKey);
+  settingCheckbox.addEventListener("change", async e => {
+    const settingKey = e.target.id;
+    const settingValue = e.target.checked;
+
+    // 1. Save changes to local storage
+    const saveObj = { [settingKey]: settingValue };
+    chrome.storage.local.set(saveObj);
+
+    // 2. Update running tabs with the changed setting
+    const messageObj = { key: settingKey, value: settingValue };
+    chrome.tabs.query({}, tabs => {
+      tabs.forEach(tab => {
+        chrome.tabs.sendMessage(tab.id, messageObj);
+      });
+    });
+  });
+});
