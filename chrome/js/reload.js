050e6b91c29c6deb58e496d4a0a3a1b36e1495f0
Lawrence Hook <lawrencehook@gmail.com>
2021-12-12 18:51:50 -0500

    Remove play next button and thumbnail mouseover effect (chrome)

--- chrome/js/reload.js
+++ chrome/js/reload.js
@@ -7,7 +7,7 @@ const reloadButton = document.getElementById("reload_button");
 reloadButton.addEventListener("click", sendReloadMessage);
 
 async function sendReloadMessage() {
-	browser.tabs.query({ active: true, currentWindow: true }, tabs => {
-	  tabs.forEach(tab => browser.tabs.reload(tab.id));
-	});
+  browser.tabs.query({ active: true, currentWindow: true }, tabs => {
+    tabs.forEach(tab => browser.tabs.reload(tab.id));
+  });
 }
