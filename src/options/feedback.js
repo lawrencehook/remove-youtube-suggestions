dd8fb021efc0dec20b1bf288f45c358a6c5476e8
Lawrence Hook <lawrencehook@gmail.com>
2023-02-18 17:56:35 -0500

    flesh out the feedback page

--- src/options/feedback.js
+++ src/options/feedback.js
@@ -3,3 +3,12 @@ const HTML = document.documentElement;
 browser.storage.local.get('dark_mode', ({ dark_mode }) => {
 	HTML.setAttribute('dark_mode', dark_mode)
 });
+
+const extensionVersion = document.getElementById('extension-version');
+extensionVersion.innerText = browser.runtime.getManifest().version;
+
+const browserVersion = document.getElementById('browser-version');
+const path = browser.runtime.getURL('/');
+const isFirefox = path.startsWith('moz');
+const isChrome = path.startsWith('chrome');
+browserVersion.innerText = isFirefox ? 'firefox' : 'chrome';
